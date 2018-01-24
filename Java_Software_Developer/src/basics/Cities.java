8d35e5f67aae4dfbabecd95066006cf00f33639b
Petri Ryynänen <31620690+mikaelbeat@users.noreply.github.com>
2018-01-25 00:12:02 +0200

    Update

--- Java_Software_Developer/src/basics/Cities.java
+++ Java_Software_Developer/src/basics/Cities.java
@@ -7,7 +7,18 @@ public class Cities {
 	public static void main(String[] args) {
 		
 		String[] cities = {"New York","San Francisco","Miami","Dallas"};
-		System.out.println(cities[0]);
+		System.out.println(cities[0]+"\n");
+		
+		System.out.println("*******************************************\n");
+		
+		String[] countries;
+		countries = new String[3];
+		countries[0] = "USA";
+		countries[1] = "Canada";
+		countries[2] = "UK";
+		System.out.println(countries[2]+"\n");
+		
+		System.out.println("*******************************************\n");
 		
 		String[] states = new String[5];
 		states[0] = "California";
@@ -15,15 +26,38 @@ public class Cities {
 		states[2] = "New Jersey";
 		states[3] = "Texas";
 		states[4] = "Utah";
-		System.out.println(states[2]);
+		int i = 0;
 		
-		String[] countries;
+		// Do loop enters the loop THEN tests condition
+		do {
+			System.out.println("All states in array are: "+states[i]);
+			i = i + 1;
+		}while (i < 5);
 		
-		countries = new String[3];
-		countries[0] = "USA";
-		countries[1] = "Canada";
-		countries[2] = "UK";
-		System.out.println(countries[2]);
+		System.out.println("");
+		System.out.println("*******************************************\n");
+		
+		
+		// While loop tests condition first then entes loop
+		int n = 0;
+		boolean stateFound = false;
+		while (!stateFound) {
+			String state = states[n];
+			System.out.println(state);
+			if (state == "Texas") {
+				System.out.println("State found!\n");
+				stateFound = true;
+			}
+			n++;
+		}
+		
+		System.out.println("*******************************************\n");
+		
+		// For loop, best structure for iterating thought an array
+		for (int x = 0; x < 5; x++) {
+			System.out.println("At the position "+(x+1)+" is "+states[x]);
+		}
+		
 		
 	}
 
---
+++ Java_Software_Developer/src/basics/Strings.java
@@ -0,0 +1,34 @@
+package basics;
+
+public class Strings {
+
+	public static void main(String[] args) {
+
+		String booktitle;
+		String wordChoice = "Ring";
+		booktitle = "The Lord Of The Rings";
+		
+		if (booktitle.contains("Ring")) {
+			System.out.println("The booktitle contais word "+wordChoice+".\n");
+		}
+		
+		String browser = "Chrome";
+		if (browser.equalsIgnoreCase("chrome")) {
+			System.out.println("The browser is Chome.\n");
+		}
+		
+		String firstName = "FirstName";
+		String lastName = "LastName";
+		String SSN = "123456789-123A";
+		
+		// Print initials plus last 4 digits from SSN
+		System.out.print(firstName.substring(0,1)+" ");
+		System.out.print(lastName.substring(0,3)+" ");
+		System.out.println(SSN.substring(10)+"\n");
+		
+		System.out.println("There are "+SSN.length()+" digits in your SSN.");
+		
+
+	}
+
+}
