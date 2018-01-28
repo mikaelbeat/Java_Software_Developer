bb563feb75f6eb54f24dea95b15b77593d7f6f92
Petri Ryynänen <31620690+mikaelbeat@users.noreply.github.com>
2018-01-28 22:36:02 +0200

    Update

--- Java_Software_Developer/src/oop/BankAccount.java
+++ Java_Software_Developer/src/oop/BankAccount.java
@@ -3,22 +3,34 @@ package oop;
 public class BankAccount {
 	
 	String accountNumber;
-	String routingNumber;
+	
+	// Static --> Belongs to the CLASS not the OBJECT instance
+	// Final --> Constant, cannot even be overwritten 
+	static final String routingNumber = "888";
 	String name;
 	String ssn;
 	String accountType;
+	double balance = 0;
 	
 	BankAccount() {
 		System.out.println("New account created.\n");
 	}
 	
 	BankAccount(String accountType){
-		System.out.println("New account "+accountType+" created.\n");
+		System.out.println("Created account: "+accountType+".\n");
 	}
 	
 	BankAccount(String accountType, double initDeposit){
-		System.out.println("New accout "+accountType+" created.");
-		System.out.println("Initial deposit is "+initDeposit+"€.\n");
+		System.out.println("Created account: "+accountType+".\n");
+		System.out.println("Initial deposit "+initDeposit+"€ done.\n");
+		if (initDeposit < 1000) {
+			String message = "ERROR: Minimum deposit must be at least 1000€.\n";
+			System.out.println(message);
+		} else {
+			String message = "Thanks for your initial deposit of "+initDeposit+"€.\n";
+			System.out.println(message);
+		}
+		balance+=initDeposit;
 	}
 	
 	void deposit() {
@@ -30,7 +42,7 @@ public class BankAccount {
 	}
 	
 	void checkBalance() {
-		
+		System.out.println("Balance is "+balance+"€.\n");
 	}
 	
 	void getStatus() {
--- Java_Software_Developer/src/oop/BankAccountApp.java
+++ Java_Software_Developer/src/oop/BankAccountApp.java
@@ -5,11 +5,19 @@ public class BankAccountApp {
 	public static void main(String[] args) {
 		
 		BankAccount account1 = new BankAccount();
+		account1.accountNumber = "1234";
 		
-		BankAccount account2 = new BankAccount("checking account");
+		BankAccount account2 = new BankAccount("Checking account");
+		account2.accountNumber = "4567";
 		
-		BankAccount account3 = new BankAccount("savings account", 5000);
-
+		BankAccount account3 = new BankAccount("Savings account", 10000);
+		account3.accountNumber = "8910";
+		account3.checkBalance();
+		
+		System.out.println(account1.routingNumber);
+		System.out.println(account2.routingNumber);
+		System.out.println(account3.routingNumber);
+		
 	}
 
 }
